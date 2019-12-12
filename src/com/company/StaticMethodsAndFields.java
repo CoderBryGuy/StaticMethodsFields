892a201ed3b6b49ef045110c929f83257d175700
Bryan <bryan.dov.bergman@gmail.com>
2019-12-12 20:43:48 +0200

    Initial commit

---
+++ src/com/company/StaticMethodsAndFields.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class StaticMethodsAndFields {
+    public static  int myNumber = 10;
+
+    public static void main(String[] args) {
+
+        System.out.println(myNumber);
+
+
+    }
+}
